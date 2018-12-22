9b9a970f0713bdf24eafcdbc79bc181650d2caba
juanseguravasco <jsegura@cipfpbatoi.es>
2018-12-22 21:04:12 +0100

    Primer commit

---
+++ src/lib/API.js
@@ -0,0 +1,62 @@
+const API_URL = 'http://localhost:3000/';
+
+const myId=1;
+
+import axios from 'axios'
+
+export default {
+    getTable(table, query) {
+        if (query) {
+            let queryString = '';
+            for (let i in query) {
+                queryString += i + '=' + query[i] + '&';
+            }
+            queryString = queryString.substr(0, queryString.length - 1);
+            return axios.get(API_URL + table + '?' + queryString);
+        } else {
+            return axios.get(API_URL + table);
+        }
+    },
+    getItem(table, id = myId) {
+        return axios.get(API_URL + table + '/' + id);
+    },
+    delItem(table, id) {
+        return axios.delete(API_URL + table + '/' + id);
+    },
+    saveItem(table, item) {
+        return axios.post(API_URL + table, item);
+    },
+    updateItem(table, id, item) {
+        return axios.put(API_URL + table + '/' + id, item);
+    },
+    getUser(item) {
+        // prova
+        return new Promise(function(resolve, reject) {
+            resolve({
+                data: {
+                    id:5,
+                    user:item.user,
+                    rol:2,
+                    token:'asdad6acguas6utash768a'
+                }    
+            })
+        });
+        return axios.post(API_URL + 'users/', item)
+    },
+    saveUser(item) {
+        // prova
+        return response={
+            data: {
+                id:5,
+                user:item.user,
+                rol:2,
+                token:'asdad6acguas6utash768a'
+            }
+        }
+        // No se usa saveItem porque hay que guardarlo en Users y en Alumnos/Empresas
+        return axios.post(API_URL + table, item);
+    },
+    sendMail(mail) {
+        return axios.post(API_URL + '/mail', mail);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
+import axios from 'axios'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// Para pasar siempre el token en las peticiones axios
+const USERAUTH = sessionStorage.getItem('user-data')
+if (USERAUTH) {
+  axios.defaults.headers.common['Authorization'] = JSON.parse(USERAUTH).token;
+}
 
 new Vue({
   router,
