81096166ec01fb2b4a60cb394178e6ac39eeb378
igomis <ignasi.gomis.mullor@gmail.com>
2019-02-22 17:51:30 +0100

    rutas

--- routes/web.php
+++ routes/web.php
@@ -17,37 +17,37 @@ $router->group(['prefix' => 'api'], function ($router) {
     $router->get('/users','UserController@index');
     $router->get('/users/{id}','UserController@show');
     $router->post('/users','UserController@store');
-    $router->put('/users','UserController@update');
+    $router->put('/users/{id}','UserController@update');
     $router->delete('/users','UserController@destroy');
     
     $router->get('/ciclos','CicloController@index');
     $router->get('/ciclos/{id}','CicloController@show');
     $router->post('/ciclos','CicloController@store');
-    $router->put('/ciclos','CicloController@update');
+    $router->put('/ciclos/{id}','CicloController@update');
     $router->delete('/ciclos','CicloController@destroy');
     
     $router->get('/alumnes','AlumneController@index');
     $router->get('/alumnes/{id}','AlumneController@show');
     $router->post('/alumnes','AlumneController@store');
-    $router->put('/alumnes','AlumneController@update');
+    $router->put('/alumnes/{id}','AlumneController@update');
     $router->delete('/alumnes','AlumneController@destroy');
     
     $router->get('/empresas','EmpresaController@index');
     $router->get('/empresas/{id}','EmpresaController@show');
     $router->post('/empresas','EmpresaController@store');
-    $router->put('/empresas','EmpresaController@update');
+    $router->put('/empresas/{id}','EmpresaController@update');
     $router->delete('/empresas','EmpresaController@destroy');
     
     $router->get('/ofertas','OfertaController@index');
     $router->get('/ofertas/{id}','OfertaController@show');
     $router->post('/ofertas','OfertaController@store');
-    $router->put('/ofertas','OfertaController@update');
+    $router->put('/ofertas/{id}','OfertaController@update');
     $router->delete('/ofertas','OfertaController@destroy');
     
     $router->get('/responsables','ResponsableController@index');
     $router->get('/responsables/{id}','ResponsableController@show');
     $router->post('/responsables','ResponsableController@store');
-    $router->put('/responsables','ResponsableController@update');
+    $router->put('/responsables/{id}','ResponsableController@update');
     $router->delete('/responsables','ResponsableController@destroy'); 
     
     
